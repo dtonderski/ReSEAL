0e25a25edb9b99b10697c61866a647b31bfb3472
Dominic Wong <dominicwong999@gmail.com>
2023-03-14 15:21:07 +0100

    Initialize conda at build

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -4,3 +4,7 @@ pip install --upgrade pip setuptools wheel
 python3 -m pip install --user --default-timeout=100 --use-deprecated=legacy-resolver -r .devcontainer/requirements-dev.txt
 python3 -m pip install --user --default-timeout=100 --use-deprecated=legacy-resolver -r requirements.txt
 python3 -m pip install -e .
+
+conda init
+source ~/.bashrc
+source activate habitat
