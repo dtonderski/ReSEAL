846dd2aa4665fcc45754fc971ffb9b8dae23fb20
Dominic Wong <dominicwong999@gmail.com>
2023-03-14 15:12:53 +0100

    Setup dev environment

---
+++ .devcontainer/post_create.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pip install --upgrade pip setuptools wheel
+python3 -m pip install --user --default-timeout=100 --use-deprecated=legacy-resolver -r .devcontainer/requirements-dev.txt
+python3 -m pip install --user --default-timeout=100 --use-deprecated=legacy-resolver -r requirements.txt
+python3 -m pip install -e .
