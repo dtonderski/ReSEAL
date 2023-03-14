1a17b65f6ae67ce097cc03c956fa238bd06ec2ed
Dominic Wong <dominicwong999@gmail.com>
2023-03-14 15:37:37 +0100

    Small fix in post_create.sh

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-pip install --upgrade pip setuptools wheel
-python3 -m pip install --user --default-timeout=100 --use-deprecated=legacy-resolver -r .devcontainer/requirements-dev.txt
-python3 -m pip install --user --default-timeout=100 --use-deprecated=legacy-resolver -r requirements.txt
-python3 -m pip install -e .
-
 conda init
 source ~/.bashrc
 source activate habitat
+
+python3 -m pip install --user --default-timeout=100 --use-deprecated=legacy-resolver -r .devcontainer/requirements-dev.txt
+python3 -m pip install --user --default-timeout=100 --use-deprecated=legacy-resolver -r requirements.txt
+python3 -m pip install -e .
