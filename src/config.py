516532d787716173590cabd70b0f590b8e5a99a0
dtonderski <dtonderski@gmail.com>
2023-03-25 20:10:19 +0000

    Added egocentric map origin offset to config

--- src/config.py
+++ src/config.py
@@ -5,5 +5,6 @@ def default_map_builder_cfg():
     map_builder_cfg = CfgNode()
     map_builder_cfg.RESOLUTION = 1.0  # cm per pixel
     map_builder_cfg.EGOCENTRIC_MAP_SHAPE = (500, 500, 500)  # (x, y, z) in pixel
+    map_builder_cfg.EGOCENTRIC_MAP_ORIGIN_OFFSET = (250, 250, 250)  # (x, y, z) in pixel
     map_builder_cfg.NUM_SEMANTIC_CLASSES = 10
     return map_builder_cfg
