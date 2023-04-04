3d93565ca87a7b3161c82a361de12623b8bcd8a9
dtonderski <dtonderski@gmail.com>
2023-04-04 03:20:53 +0000

    Added support for semantic sensors. Now, if the scene supports a semantic sensor, the simulator will automatically include it. Examples are shown in Generate_trajectory and Visualise_trajectory notebooks.

--- src/config.py
+++ src/config.py
@@ -1,10 +1,35 @@
+from pathlib import Path
 from yacs.config import CfgNode
 
 
-def default_map_builder_cfg():
+def default_map_builder_cfg() -> CfgNode:
     map_builder_cfg = CfgNode()
     map_builder_cfg.RESOLUTION = 1.0  # cm per pixel
     map_builder_cfg.EGOCENTRIC_MAP_SHAPE = (500, 500, 500)  # (x, y, z) in pixel
     map_builder_cfg.EGOCENTRIC_MAP_ORIGIN_OFFSET = (250, 250, 250)  # (x, y, z) in pixel
     map_builder_cfg.NUM_SEMANTIC_CLASSES = 10
     return map_builder_cfg
+
+def default_data_paths_cfg() -> CfgNode:
+    data_paths_cfg = CfgNode()
+    data_paths_cfg.RAW_DATA_DIR = str(Path("data", "raw"))
+    data_paths_cfg.ANNOTATED_SCENE_CONFIG_PATH_IN_SPLIT = str(Path("scene_datasets", "hm3d",
+                                                                   "hm3d_annotated_basis.scene_dataset_config.json"))
+    data_paths_cfg.BASIS_SCENE_DATASET_CONFIG_PATH_IN_SPLIT = str(Path("scene_datasets", "hm3d",
+                                                                       "hm3d_basis.scene_dataset_config.json"))
+    data_paths_cfg.INTERIM_DATA_DIR = str(Path("data", "interim"))
+    data_paths_cfg.TRAJECTORIES_DIR = str(Path(data_paths_cfg.INTERIM_DATA_DIR, "trajectories"))
+    
+    return data_paths_cfg
+
+def default_sim_cfg() -> CfgNode:
+    sim_cfg = CfgNode()
+    sim_cfg.WIDTH = 256 #px
+    sim_cfg.HEIGHT = 256 #px
+    sim_cfg.SENSOR_HEIGHT = 0.88 #m
+    sim_cfg.FORWARD_MOVE_DISPLACEMENT = 0.25 #m
+    sim_cfg.TURN_ANGLE_DISPLACEMENT = 30 #deg
+    sim_cfg.DEFAULT_AGENT_ID = 0
+    sim_cfg.DEFAULT_POSITION = [-0.6, 0.0, 0.0] #m
+    return sim_cfg
+    
--- src/data/scene.py
+++ src/data/scene.py
@@ -1,76 +1,42 @@
-import habitat_sim
+import json
+from typing import Tuple
+from pathlib import Path
+
 import numpy as np
+import habitat_sim
+from yacs.config import CfgNode
 
-def make_cfg(settings):
-    sim_cfg = habitat_sim.SimulatorConfiguration()
-    sim_cfg.gpu_device_id = 0
-    sim_cfg.scene_id = settings["scene"]
-    sim_cfg.scene_dataset_config_file = settings["scene_dataset"]
-    sim_cfg.enable_physics = settings["enable_physics"]
+from ..config import default_data_paths_cfg, default_sim_cfg
 
-    # Note: all sensors must have the same resolution
-    sensor_specs = []
+def initialize_sim(scene_split: str,
+                   scene_id: str,
+                   data_paths_cfg: CfgNode = None,
+                   sim_cfg: CfgNode = None,
+                   verbose:bool=False) -> habitat_sim.Simulator:
+    """ Initialize the simulator from a given scene
 
-    color_sensor_spec = habitat_sim.CameraSensorSpec()
-    color_sensor_spec.uuid = "color_sensor"
-    color_sensor_spec.sensor_type = habitat_sim.SensorType.COLOR
-    color_sensor_spec.resolution = [settings["height"], settings["width"]]
-    color_sensor_spec.position = [0.0, settings["sensor_height"], 0.0]
-    color_sensor_spec.sensor_subtype = habitat_sim.SensorSubType.PINHOLE
-    sensor_specs.append(color_sensor_spec)
+    Args:
+        scene_split (str): 'train', 'val', or 'minival'.
+        scene_id (str): string of the form [0-9]{5}-[0-z]{11}, for example 00800-TEEsavR23oF.
+        data_paths_cfg (CfgNode, optional): data path yacs config. Defaults to None. \
+            If None, uses default_data_paths_cfg()
+        sim_cfg (CfgNode, optional): sim yacs config. Defaults to None. If None, uses default_sim_cfg().
+        verbose (bool, optional): if set, prints agent position and rotation after init. Defaults to False.
 
-    depth_sensor_spec = habitat_sim.CameraSensorSpec()
-    depth_sensor_spec.uuid = "depth_sensor"
-    depth_sensor_spec.sensor_type = habitat_sim.SensorType.DEPTH
-    depth_sensor_spec.resolution = [settings["height"], settings["width"]]
-    depth_sensor_spec.position = [0.0, settings["sensor_height"], 0.0]
-    depth_sensor_spec.sensor_subtype = habitat_sim.SensorSubType.PINHOLE
-    sensor_specs.append(depth_sensor_spec)
+    Returns:
+        habitat_sim.Simulator: simulator with the current scene loaded. The agent will have a semantic sensor if \
+            scene is part of HM3D-SEM.
+    """
+    if sim_cfg is None:
+        sim_cfg = default_sim_cfg()
 
-    # Here you can specify the amount of displacement in a forward action and the turn angle
-    agent_cfg = habitat_sim.agent.AgentConfiguration()
-    agent_cfg.sensor_specifications = sensor_specs
-    agent_cfg.action_space = {
-        "move_forward": habitat_sim.agent.ActionSpec(
-            "move_forward", habitat_sim.agent.ActuationSpec(amount=0.25)
-        ),
-        "turn_left": habitat_sim.agent.ActionSpec(
-            "turn_left", habitat_sim.agent.ActuationSpec(amount=30.0)
-        ),
-        "turn_right": habitat_sim.agent.ActionSpec(
-            "turn_right", habitat_sim.agent.ActuationSpec(amount=30.0)
-        ),
-    }
-
-    return habitat_sim.Configuration(sim_cfg, [agent_cfg])
-
-
-def initialize_scene(scene:str,
-                     scene_dataset:str="data/raw/train/scene_datasets/hm3d/hm3d_basis.scene_dataset_config.json",
-                     verbose:bool=False):
-    rgb_sensor = True  # @param {type:"boolean"}
-    depth_sensor = True  # @param {type:"boolean"}
-
-    sim_settings = {
-        "width": 256,  # Spatial resolution of the observations
-        "height": 256,
-        "scene": scene,  # Scene path
-        "scene_dataset": scene_dataset,  # the scene dataset configuration files
-        "default_agent": 0,
-        "sensor_height": 0.88,  # Height of sensors in meters
-        "color_sensor": rgb_sensor,  # RGB sensor
-        "depth_sensor": depth_sensor,  # Depth sensor
-        "seed": 1,  # used in the random navigation
-        "enable_physics": False,  # kinematics only
-    }
-
-    cfg = make_cfg(sim_settings)
-    sim = habitat_sim.Simulator(cfg)
+    habitat_sim_cfg = make_habitat_sim_cfg(scene_split, scene_id, data_paths_cfg, sim_cfg)
+    sim = habitat_sim.Simulator(habitat_sim_cfg)
 
     # Set agent state
-    agent = sim.initialize_agent(sim_settings["default_agent"])
+    agent = sim.initialize_agent(sim_cfg.DEFAULT_AGENT_ID)
     agent_state = habitat_sim.AgentState()
-    agent_state.position = np.array([-0.6, 0.0, 0.0])  # world space
+    agent_state.position = np.array(sim_cfg.DEFAULT_POSITION)  # world space
     agent.set_state(agent_state)
 
     # Get agent state
@@ -79,3 +45,146 @@ def initialize_scene(scene:str,
         print("agent_state: position", agent_state.position, "rotation", agent_state.rotation)
 
     return sim
+
+def check_if_semantic_sensor_used(sim: habitat_sim.Simulator) -> bool:
+    """ Utility function that checks whether the first (and usually only) agent in the scene has a semantic sensor.
+
+    Args:
+        sim (habitat_sim.Simulator): the initialized simulator
+
+    Returns:
+        bool: True if the first agent has a semantic sensor, False otherwise.
+    """
+    return any(x.sensor_type == habitat_sim.SensorType.SEMANTIC
+                for x in sim.config.agents[0].sensor_specifications)
+
+
+def make_habitat_sim_cfg(scene_split: str,
+                         scene_id: str,
+                         data_paths_cfg: CfgNode = None,
+                         sim_cfg: CfgNode = None) -> habitat_sim.Configuration:
+    """ Creates a habitat_sim.Configuration object for a given scene
+
+    Args:
+        scene_split (str): 'train', 'val', or 'minival'.
+        scene_id (str): string of the form [0-9]{5}-[0-z]{11}, for example 00800-TEEsavR23oF.
+        data_paths_cfg (CfgNode, optional): data path yacs config. Defaults to None. \
+            If None, uses default_data_paths_cfg()
+        sim_cfg (CfgNode, optional): sim yacs config. Defaults to None. If None, uses default_sim_cfg().
+
+    Returns:
+        habitat_sim.Configuration: the configuration object for the given scene.
+    """
+    if sim_cfg is None:
+        sim_cfg = default_sim_cfg()
+
+    scene_path, scene_dataset_config_path, use_semantic_sensor = get_scene_info(scene_split, scene_id, data_paths_cfg)
+
+
+    habitat_sim_cfg = habitat_sim.SimulatorConfiguration()
+    habitat_sim_cfg.gpu_device_id = 0
+    habitat_sim_cfg.scene_id = str(scene_path)
+    habitat_sim_cfg.scene_dataset_config_file = str(scene_dataset_config_path)
+    habitat_sim_cfg.enable_physics = False
+
+    # Note: all sensors must have the same resolution
+    sensor_specs = []
+    sensor_specs.append(get_sensor_spec("color_sensor", sim_cfg))
+    sensor_specs.append(get_sensor_spec("depth_sensor", sim_cfg))
+
+    if use_semantic_sensor:
+        sensor_specs.append(get_sensor_spec("semantic_sensor", sim_cfg))
+
+    # Here you can specify the amount of displacement in a forward action and the turn angle
+    agent_cfg = habitat_sim.agent.AgentConfiguration()
+    agent_cfg.sensor_specifications = sensor_specs
+    agent_cfg.action_space = {
+        "move_forward": habitat_sim.agent.ActionSpec(
+            "move_forward", habitat_sim.agent.ActuationSpec(amount=sim_cfg.FORWARD_MOVE_DISPLACEMENT)
+        ),
+        "turn_left": habitat_sim.agent.ActionSpec(
+            "turn_left", habitat_sim.agent.ActuationSpec(amount=sim_cfg.TURN_ANGLE_DISPLACEMENT)
+        ),
+        "turn_right": habitat_sim.agent.ActionSpec(
+            "turn_right", habitat_sim.agent.ActuationSpec(amount=sim_cfg.TURN_ANGLE_DISPLACEMENT)
+        ),
+    }
+
+    return habitat_sim.Configuration(habitat_sim_cfg, [agent_cfg])
+
+
+def get_scene_info(scene_split: str,
+                   scene_id: str,
+                   data_paths_cfg: CfgNode = None) -> Tuple[Path, Path, bool]:
+    """Gets the scene path, scene dataset config path, and whether the scene uses a semantic sensor.
+
+    Args:
+        scene_split (str): 'train', 'val', or 'minival'.
+        scene_id (str): string of the form [0-9]{5}-[0-z]{11}, for example 00800-TEEsavR23oF.
+        data_paths_cfg (CfgNode, optional): data path yacs config. Defaults to None. \
+            If None, uses default_data_paths_cfg()
+
+    Returns:
+        Tuple[Path, Path, bool]: the path to the .glb file, the path to the .json scene dataset config file, and \
+            a boolean indicating whether the scene supports a semantic sensor.
+    """
+
+    if data_paths_cfg is None:
+        data_paths_cfg = default_data_paths_cfg()
+
+    annotated_scene_config_path: Path = Path(data_paths_cfg.RAW_DATA_DIR, scene_split,
+                                            data_paths_cfg.ANNOTATED_SCENE_CONFIG_PATH_IN_SPLIT)
+
+    with annotated_scene_config_path.open(encoding='utf-8') as file:
+        annotated_scene_config = json.load(file)
+
+    annotated_scene_set = {Path(x).parents[0] for x in annotated_scene_config["stages"]["paths"][".glb"]}
+    use_semantic_sensor = Path(scene_split, scene_id) in annotated_scene_set
+
+    scene_id_without_index = scene_id.split('-')[1]
+    if use_semantic_sensor:
+        scene_dataset_config_path = Path(data_paths_cfg.RAW_DATA_DIR, scene_split,
+                                        data_paths_cfg.ANNOTATED_SCENE_CONFIG_PATH_IN_SPLIT)
+    else:
+        scene_dataset_config_path = Path(data_paths_cfg.RAW_DATA_DIR, scene_split,
+                                        data_paths_cfg.BASIS_SCENE_DATASET_CONFIG_PATH_IN_SPLIT)
+
+    scene_filename = f'{scene_id_without_index}.basis.glb'
+
+    scene_path = Path(data_paths_cfg.RAW_DATA_DIR, scene_split, 
+                    "scene_datasets", "hm3d", scene_split, scene_id, scene_filename)
+
+    return scene_path, scene_dataset_config_path, use_semantic_sensor
+
+
+def get_sensor_spec(sensor_type: str, sim_cfg: CfgNode = None) -> habitat_sim.CameraSensorSpec:
+    """ Creates a sensor spec for the given sensor type.
+
+    Args:
+        sensor_type (str): "color_sensor", "depth_sensor", or "semantic_sensor".
+        sim_cfg (CfgNode, optional): yacs simulator configuration. Defaults to None. If None, default_sim_cfg() is used.
+
+    Returns:
+        habitat_sim.CameraSensorSpec: sensor spec for the given sensor type.
+    """
+
+    assert sensor_type in ["color_sensor", "depth_sensor", "semantic_sensor"]
+
+    if sim_cfg is None:
+        sim_cfg = default_sim_cfg()
+
+    sensor_spec: habitat_sim.CameraSensorSpec = habitat_sim.CameraSensorSpec()
+
+    if sensor_type == "color_sensor":
+        sensor_spec.sensor_type = habitat_sim.SensorType.COLOR
+    elif sensor_type == "depth_sensor":
+        sensor_spec.sensor_type = habitat_sim.SensorType.DEPTH
+    else:
+        sensor_spec.sensor_type = habitat_sim.SensorType.SEMANTIC
+
+    sensor_spec.uuid = sensor_type
+    sensor_spec.resolution = [sim_cfg.HEIGHT, sim_cfg.WIDTH]
+    sensor_spec.position = [0.0, sim_cfg.SENSOR_HEIGHT, 0.0]
+    sensor_spec.sensor_subtype = habitat_sim.SensorSubType.PINHOLE
+
+    return sensor_spec
