9bd8d93ca855e994d8f9a0859ab730a1a9c8a8a2
dtonderski <dtonderski@gmail.com>
2023-04-07 19:56:22 +0000

    Fixed some datatypes

--- src/utils/datatypes.py
+++ src/utils/datatypes.py
@@ -5,11 +5,11 @@ import quaternion # pylint: disable=unused-import
 
 
 SemanticMap3D = NDArray[Shape["NumPixelsX, NumPixelsY, NumPixelsZ, NumChannels"], Float]
-Coordinate2D = Tuple[int, int]
-Coordinate3D = Tuple[int, int, int]
+Coordinate2D = Tuple[float, float]
+Coordinate3D = Tuple[float, float, float]
 CoordinatesMapping2Dto3D = Mapping[Coordinate2D, Coordinate3D]
 CoordinatesMapping3Dto3D = Mapping[Coordinate3D, Coordinate3D]
-GridIndex2D = Tuple[int, int, int]
+GridIndex2D = Tuple[int, int]
 GridIndex3D = Tuple[int, int, int]
 
 SemanticMap2D = NDArray[Shape["Height, Width, NumChannels"], Float]
