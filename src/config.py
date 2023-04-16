97103f91f0b983211beb9ee43c96eaad1943a12a
akichutkin <akichutkin1@gmail.com>
2023-04-16 14:51:17 +0000

    Added 2D to 3D mapping, changed config and mapping

--- src/config.py
+++ src/config.py
@@ -29,6 +29,8 @@ def default_sim_cfg() -> CfgNode:
     sim_cfg.TURN_ANGLE_DISPLACEMENT = 30 #deg
     sim_cfg.DEFAULT_AGENT_ID = 0
     sim_cfg.DEFAULT_POSITION = [-0.6, 0.0, 0.0] #m
+    sim_cfg.HFOV = 90 #deg
+
     return sim_cfg
 
 def default_sensor_cfg() -> CfgNode:
