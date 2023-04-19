7f7d10cb75e44af933c746dbd825403ecf69259e
Dominic Wong <dominicwong999@gmail.com>
2023-04-19 12:49:38 +0200

    style: format python files

--- src/config.py
+++ src/config.py
@@ -11,17 +11,21 @@ def default_map_builder_cfg() -> CfgNode:
     map_builder_cfg.NUM_SEMANTIC_CLASSES = 10
     return map_builder_cfg
 
+
 def default_data_paths_cfg() -> CfgNode:
     data_paths_cfg = CfgNode()
     data_paths_cfg.RAW_DATA_DIR = str(Path("data", "raw"))
-    data_paths_cfg.ANNOTATED_SCENE_CONFIG_PATH_IN_SPLIT = str(Path("scene_datasets", "hm3d",
-                                                                   "hm3d_annotated_basis.scene_dataset_config.json"))
-    data_paths_cfg.BASIS_SCENE_DATASET_CONFIG_PATH_IN_SPLIT = str(Path("scene_datasets", "hm3d",
-                                                                       "hm3d_basis.scene_dataset_config.json"))
+    data_paths_cfg.ANNOTATED_SCENE_CONFIG_PATH_IN_SPLIT = str(
+        Path("scene_datasets", "hm3d", "hm3d_annotated_basis.scene_dataset_config.json")
+    )
+    data_paths_cfg.BASIS_SCENE_DATASET_CONFIG_PATH_IN_SPLIT = str(
+        Path("scene_datasets", "hm3d", "hm3d_basis.scene_dataset_config.json")
+    )
     data_paths_cfg.INTERIM_DATA_DIR = str(Path("data", "interim"))
     data_paths_cfg.TRAJECTORIES_DIR = str(Path(data_paths_cfg.INTERIM_DATA_DIR, "trajectories"))
 
     return data_paths_cfg
+
 
 def default_sim_cfg() -> CfgNode:
     sim_cfg = CfgNode()
@@ -29,8 +33,8 @@ def default_sim_cfg() -> CfgNode:
     sim_cfg.FORWARD_MOVE_DISPLACEMENT = 0.25 #m
     sim_cfg.TURN_ANGLE_DISPLACEMENT = 30 #deg
     sim_cfg.DEFAULT_AGENT_ID = 0
-    sim_cfg.DEFAULT_POSITION = [-0.6, 0.0, 0.0] #m
-    sim_cfg.HFOV = 90 #deg
+    sim_cfg.DEFAULT_POSITION = [-0.6, 0.0, 0.0]  # m
+    sim_cfg.HFOV = 90  # deg
 
     return sim_cfg
 
