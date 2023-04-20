07dad3e4da3abb602eb897ac27d01be8b72fa05f
Dominic Wong <dominicwong999@gmail.com>
2023-04-20 17:50:31 +0000

    fix: local policy returns one action at a time

--- src/model/action/local_policy.py
+++ src/model/action/local_policy.py
@@ -1,9 +1,7 @@
 from abc import ABC, abstractmethod
-from typing import List, Optional
-from warnings import warn
+from typing import Optional
 
 import numpy as np
-from habitat_sim.errors import GreedyFollowerError
 from habitat_sim.agent import Agent
 from habitat_sim.nav import GreedyGeodesicFollower, PathFinder
 from yacs.config import CfgNode
@@ -13,25 +11,25 @@ from ...utils import datatypes
 
 class LocalPolicy(ABC):
     @abstractmethod
-    def __call__(self, global_goal: datatypes.Coordinate3D) -> List[Optional[datatypes.AgentAction]]:
+    def __call__(self, global_goal: datatypes.Coordinate3D) -> Optional[datatypes.AgentAction]:
         pass
 
 
 class GreedyLocalPolicy(LocalPolicy):
     def __init__(self, local_policy_cfg: CfgNode, navmesh_filepath: str, agent: Agent):
-        self._max_num_steps = local_policy_cfg.MAX_NUM_STEPS
         self._agent = agent
+        self._distance_threshold = local_policy_cfg.DISTANCE_THRESHOLD
         self._path_finder = PathFinder()
         self._path_finder.load_nav_mesh(navmesh_filepath)
         self._planner = GreedyGeodesicFollower(self._path_finder, self._agent)
 
-    def __call__(self, global_goal: datatypes.Coordinate3D) -> List[Optional[datatypes.AgentAction]]:
+    def __call__(self, global_goal: datatypes.Coordinate3D) -> Optional[datatypes.AgentAction]:
+        if self._is_agent_within_threshold(global_goal):
+            return None
         global_goal_arr = np.array(global_goal)
-        try:
-            actions = self._planner.find_path(global_goal_arr)
-        except GreedyFollowerError:
-            warn("Greedy follower failed to find path. Returning no actions", RuntimeWarning)
-            return [None]
-        if len(actions) > self._max_num_steps + 1:
-            actions = actions[: self._max_num_steps] + [None]
-        return actions
+        return self._planner.next_action_along(global_goal_arr)
+
+    def _is_agent_within_threshold(self, global_goal: datatypes.Coordinate3D) -> bool:
+        agent_position = self._agent.state.position
+        global_goal_arr = np.array(global_goal)
+        return np.linalg.norm(agent_position - global_goal_arr) < self._distance_threshold
