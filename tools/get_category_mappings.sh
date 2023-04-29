7396fd2f469e1b243e7a70318f43190496b34ded
dtonderski <dtonderski@gmail.com>
2023-04-29 09:24:50 +0000

    Added unnecessary get_category_mappings, as the mappings are in the data/raw folder anyway

---
+++ tools/get_category_mappings.sh
@@ -0,0 +1,5 @@
+wget -O data/raw/hm3dsem_category_mappings.tsv \
+    https://raw.githubusercontent.com/facebookresearch/habitat-sim/main/data/hm3d_semantics/hm3dsem_category_mappings.tsv
+
+wget -O data/raw/mpcat40.tsv \
+    https://raw.githubusercontent.com/niessner/Matterport/master/metadata/mpcat40.tsv
