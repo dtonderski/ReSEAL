6f72f7f12e1232df0d843c9638508e7b1d7d6578
dtonderski <dtonderski@gmail.com>
2023-04-30 01:35:13 +0000

    Added helper function to convert semantic map to categories

--- src/utils/misc.py
+++ src/utils/misc.py
@@ -2,6 +2,7 @@ from typing import Callable
 
 import numpy as np
 from nptyping import Int, NDArray, Shape
+from src.utils.datatypes import SemanticMap3D
 
 
 def sorted_dict_by_value(dictionary):
@@ -31,3 +32,17 @@ def get_semantic_map(
     semantic_map[np.arange(semantic_map_reseal_indices.size), semantic_map_reseal_indices.flatten()] = 1
     semantic_map = semantic_map.reshape(saved_semantics.shape + (num_semantic_classes + 1,))[:,:,1:]
     return semantic_map
+
+def semantic_map_to_categorical_map(
+        semantic_map: SemanticMap3D
+    ) -> NDArray[Shape["NumPixelsX, NumPixelsY, NumPixelsZ"], Int]:
+    """ Function to extract the categories for a semantic map. If a voxel has no semantic information, it gets category
+        0, otherwise, it gets a category between 1 and NUM_SEMANTIC_CLASSES.
+
+    Args:
+        semantic_map (SemanticMap3D): one-hot encoded semantic map.
+
+    Returns:
+        NDArray[Shape["Height, Width"], Int]: semantic categories of the semantic map.
+    """
+    return (semantic_map[:,:,:,1:].argmax(axis=-1) + semantic_map[:,:,:,1:].sum(axis=-1))
