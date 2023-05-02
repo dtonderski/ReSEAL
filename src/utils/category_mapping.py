48ca599d1e7bd820f0aaf681f1e460cab3ea7476
dtonderski <dtonderski@gmail.com>
2023-05-02 02:58:06 +0000

    Work on label map, label pipeline, and visualization

--- src/utils/category_mapping.py
+++ src/utils/category_mapping.py
@@ -3,6 +3,7 @@ from typing import Callable, Dict
 
 import numpy as np
 from matplotlib.colors import to_rgb
+from nptyping import NDArray, Shape, Float
 from yacs.config import CfgNode
 
 from ..config import default_data_paths_cfg
@@ -90,15 +91,14 @@ def get_scene_index_to_reseal_index_map(semantic_info_file_path: str, data_paths
 def get_scene_index_to_reseal_index_map_vectorized(semantic_info_file_path, data_paths_cfg:CfgNode = None) -> Callable:
     return np.vectorize(get_scene_index_to_reseal_index_map(semantic_info_file_path, data_paths_cfg).get)
 
-def get_reseal_color_converter(data_paths_cfg: CfgNode=None) -> Callable:
-    """ Get a numpy vectorized dictionary mapping from reseal index to reseal color. Can be chained with\
-        get_scene_index_to_reseal_index_map_vectorized to get the color for each pixel in a scene.
+def get_reseal_color_dict(data_paths_cfg: CfgNode = None) -> Dict[int, NDArray[Shape["3"], Float]]:
+    """ Get a dictionary mapping from reseal index to reseal color.
 
     Args:
         data_paths_cfg (CfgNode, optional): optional data_paths configuration. If none, uses default_data_paths_cfg().
 
     Returns:
-        Callable: numpy vectorized dictionary mapping from reseal index to reseal color.
+        Dict[str, Tuple[int, int, int]]: dictionary mapping from reseal index to reseal color
     """
     data_paths_cfg = default_data_paths_cfg() if data_paths_cfg is None else data_paths_cfg
 
@@ -108,5 +108,17 @@ def get_reseal_color_converter(data_paths_cfg: CfgNode=None) -> Callable:
         next(iter(reader))
         for row in reader:
             reseal_index_to_color[int(row[0])] = np.array(to_rgb(f"#{row[2]}"))
+    return reseal_index_to_color
 
+def get_reseal_color_converter(data_paths_cfg: CfgNode=None) -> Callable:
+    """ Get a numpy vectorized dictionary mapping from reseal index to reseal color. Can be chained with\
+        get_scene_index_to_reseal_index_map_vectorized to get the color for each pixel in a scene.
+
+    Args:
+        data_paths_cfg (CfgNode, optional): optional data_paths configuration.
+
+    Returns:
+        Callable: numpy vectorized dictionary mapping from reseal index to reseal color.
+    """
+    reseal_index_to_color = get_reseal_color_dict(data_paths_cfg)
     return np.vectorize(reseal_index_to_color.get, signature='()->(n)')
---
+++ src/visualisation/semantic_map_visualization.py
@@ -0,0 +1,82 @@
+from typing import Dict, Tuple, Union
+
+import numpy as np
+import pyvista as pv
+from matplotlib.colors import ListedColormap
+from nptyping import Int, NDArray, Shape
+from yacs.config import CfgNode
+
+from ..utils.category_mapping import get_reseal_color_dict, get_reseal_name_to_reseal_index_map
+from ..utils.datatypes import LabelMap3DCategorical, SemanticMap3D
+
+
+def visualize_semantic_map(semantic_map: SemanticMap3D, object_threshold:float = 0.5,
+                           opacity:float = 0.2, data_paths_cfg: CfgNode = None) -> pv.Plotter:
+    map_for_plotting = get_map_for_plotting_from_semantic_map(semantic_map, object_threshold)
+    return visualize_map(map_for_plotting, opacity, data_paths_cfg)
+
+def visualize_categorical_label_map(label_map: LabelMap3DCategorical, opacity:float = 0.2, 
+                                    data_paths_cfg: CfgNode = None) -> pv.Plotter:
+    map_for_plotting = get_map_for_plotting_from_label_map(label_map)
+    return visualize_map(map_for_plotting, opacity, data_paths_cfg)
+
+def visualize_map(map_for_plotting: NDArray[Shape["X,Y,Z"], Int],
+                  opacity:float = 0.2,
+                  data_paths_cfg: CfgNode = None) -> pv.Plotter:
+     
+    color_map, reseal_map = get_plotting_dicts(map_for_plotting.max(), opacity, data_paths_cfg)
+    dims = map_for_plotting.shape
+
+    grid = pv.UniformGrid()
+    grid.dimensions = tuple(np.array(dims) + 1) # type: ignore
+    grid.cell_data["values"] = map_for_plotting.flatten(order="F")  # Flatten the array in column-major order
+    grid = grid.threshold(0.001)
+
+    plotter = pv.Plotter()
+    sargs = dict(
+        title_font_size=20,
+        label_font_size=16,
+        shadow=True,
+        n_labels=0,
+        italic=True,
+        fmt="%.1f",
+        font_family="arial",
+    )
+
+    plotter.add_mesh(grid, scalars="values", cmap=color_map, annotations = reseal_map, scalar_bar_args=sargs)
+    return plotter
+
+def get_map_for_plotting_from_semantic_map(semantic_map: SemanticMap3D, object_threshold:float = 0.5
+                                           ) -> NDArray[Shape["X,Y,Z"], Int]:
+    occupancy = semantic_map[:,:,:,0]
+    map_at_index_semantics_thresholded = semantic_map[:,:,:,1:].copy()
+    map_at_index_semantics_thresholded[object_threshold < 0.5] = 0
+
+    # 0 if no semantic information, 1-6 if semantic information
+    map_at_index_categorical = (np.argmax(map_at_index_semantics_thresholded, axis=-1)
+                                + np.sum(map_at_index_semantics_thresholded, axis=-1))
+
+    # 0 if no occupancy, 1 if occupied but no semantic information, 2-7 if occupied and semantic information
+    map_for_plotting = (map_at_index_categorical + occupancy)
+    return map_for_plotting
+
+def get_map_for_plotting_from_label_map(label_map: LabelMap3DCategorical) -> NDArray[Shape["X,Y,Z"], Int]:
+    return label_map.sum(axis=-1)
+
+def get_plotting_dicts(max_present_label_index: int, opacity:float = 0.4,
+                       data_paths_cfg: CfgNode = None) -> Tuple[ListedColormap, Dict[int, str]]:
+    """Get color map and label names for plotting.
+
+    Args:
+        opacity (float, optional): Not sure why, but this sets the opacity of all voxels. Defaults to 0.4.
+        data_paths_cfg (CfgNode, optional): data paths config. Defaults to None.
+
+    Returns:
+        Tuple[ListedColormap, Dict[int, str]]: color map and label names
+    """
+    color_dict = {k:v for k,v in get_reseal_color_dict(data_paths_cfg).items() if k < max_present_label_index}
+    color_dict[0] = np.array([*color_dict[0], opacity])
+    color_map = ListedColormap([*[color_dict[category] for category in color_dict]])
+
+    reseal_map = {v+1:k for k,v in get_reseal_name_to_reseal_index_map(data_paths_cfg).items()}
+    return color_map, reseal_map
