652668cde0e31aa698b6e8a39fa6bdede9084347
Dominic Wong <dominicwong999@gmail.com>
2023-05-09 23:41:05 +0200

    Install habitat-sim seperately

--- tools/build_images.sh
+++ tools/build_images.sh
@@ -12,12 +12,12 @@ docker build \
   "${BASE_DIR}"
 
 # Build the non-CUDA image
-# echo "Building non-CUDA image..."
-# docker build \
-#   --file "${BASE_DIR}/cpu/Dockerfile" \
-#   --build-arg BASE_IMAGE=ubuntu:20.04 \
-  # --tag reseal_dev_image:cpu-v1.0 \
-  # "${BASE_DIR}"
+echo "Building non-CUDA image..."
+docker build \
+  --file "${BASE_DIR}/Dockerfile" \
+  --build-arg BASE_IMAGE=ubuntu:20.04 \
+  --tag reseal_dev_image:cpu-v1.0 \
+  "${BASE_DIR}"
 
 # Display a message indicating that the build process is complete
 echo "Build complete!"
--- tools/setup_venv.sh
+++ tools/setup_venv.sh
@@ -1,9 +1,20 @@
 # /usr/bin/bash
 
+# Setup venv
 python3 -m venv .venv
 source .venv/bin/activate
+
+# Update pip and setuptool
 pip install --upgrade pip
 pip install setuptools==65.6.3
+
+# Install habitat-sim
+git clone https://github.com/facebookresearch/habitat-sim.git
+cd habitat-sim
+git checkout v0.2.3
+pip install -e .
+
+# Install requirements
 pip install --default-timeout=1000 -r requirements.txt
 pip install -r requirements-dev.txt
 
