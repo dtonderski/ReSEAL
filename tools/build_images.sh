f2cd78e509926a97a52bb40cd2964a147249794f
Dominic Wong <dominicwong999@gmail.com>
2023-05-09 22:39:26 +0200

    Added Dockerfile and script to build images

---
+++ tools/build_images.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Set the base directory where the Dockerfiles are located
+BASE_DIR="./"
+
+# Build the CUDA-enabled image
+echo "Building CUDA image..."
+docker build \
+  --file "${BASE_DIR}/Dockerfile" \
+  --build-arg BASE_IMAGE=nvcr.io/nvidia/cudagl:11.4.2-base-ubuntu20.04 \
+  --tag reseal_dev_image:cuda-v1.0 \
+  "${BASE_DIR}"
+
+# Build the non-CUDA image
+# echo "Building non-CUDA image..."
+# docker build \
+#   --file "${BASE_DIR}/cpu/Dockerfile" \
+#   --build-arg BASE_IMAGE=ubuntu:20.04 \
+  # --tag reseal_dev_image:cpu-v1.0 \
+  # "${BASE_DIR}"
+
+# Display a message indicating that the build process is complete
+echo "Build complete!"
+
--- tools/setup_venv.sh
+++ tools/setup_venv.sh
@@ -4,6 +4,6 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
 pip install setuptools==65.6.3
-pip install -r requirements.txt
+pip install --default-timeout=1000 -r requirements.txt
 pip install -r requirements-dev.txt
 
