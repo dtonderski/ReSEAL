cad03fd22adf365ebde03e9926ed1fde7db9b508
Dominic Wong <dominicwong999@gmail.com>
2023-05-11 01:07:23 +0200

    Fix problems with conda in Dockerfile

--- tools/setup_venv.sh
+++ tools/setup_venv.sh
@@ -1,18 +1,7 @@
 # /usr/bin/bash
 
-# Setup venv
-python3 -m venv .venv
-source .venv/bin/activate
-
-# Update pip and setuptool
-pip install --upgrade pip
-pip install setuptools==65.6.3
-
 # Install habitat-sim
-git clone https://github.com/facebookresearch/habitat-sim.git
-cd habitat-sim
-git checkout v0.2.3
-pip install -e .
+conda install habitat-sim withbullet headless -c conda-forge -c aihabitat
 
 # Install requirements
 pip install --default-timeout=1000 -r requirements.txt
