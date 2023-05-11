6e3e083b9ff6689e1a465ed270fc475924628f79
Dominic Wong <dominicwong999@gmail.com>
2023-05-11 03:25:06 +0200

    Image based on cudagl image builds

--- tools/build_images.sh
+++ tools/build_images.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
 # Build the CUDA-enabled image
-echo "Building CUDA image..."
+echo "Building docker image..."
 docker build \
   --file "./docker/Dockerfile.cuda" \
   --tag reseal_dev_image:cuda-v1.0 \
   "./"
-
-# Build the non-CUDA image
-# echo "Building non-CUDA image..."
-# docker build \
-#   --file "${BASE_DIR}/Dockerfile" \
-#   --build-arg BASE_IMAGE=ubuntu:20.04 \
-#   --tag reseal_dev_image:cpu-v1.0 \
-#   "${BASE_DIR}"
 
 # Display a message indicating that the build process is complete
 echo "Build complete!"
